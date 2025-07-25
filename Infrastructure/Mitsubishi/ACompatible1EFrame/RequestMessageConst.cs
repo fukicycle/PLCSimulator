95aa5c5a0e059a285af611f4a16f37cc7d272f6d
fukicycle <lego.sato.4135@gmail.com>
2025-07-25 14:22:15 +0900

    Add A compatitble 1e frame message template.

---
+++ Infrastructure/Mitsubishi/ACompatible1EFrame/RequestMessageConst.cs
@@ -0,0 +1,18 @@
+namespace Infrastructure.Mitsubishi.ACompatible1EFrame;
+
+public static class RequestMessageConst
+{
+    public const int SUB_HEADER_POSITION = 0;
+    public const int PC_NUMBER_POSITION = 1;
+    public const int ACPU_MONITOR_TIMER_L = 2;
+    public const int ACPU_MONITOR_TIMER_H = 3;
+    public const int START_DEVICE_NUMBER_1 = 4;
+    public const int START_DEVICE_NUMBER_2 = 5;
+    public const int START_DEVICE_NUMBER_3 = 6;
+    public const int START_DEVICE_NUMBER_4 = 7;
+    public const int DEVICE_NAME_L = 8;
+    public const int DEVICE_NAME_H = 9;
+    public const int DEVICE_COUNT = 10;
+    public const int EMPTY_BYTE = 11;
+    public const int MINIMUM_LENGTH = 12;
+}
